2015a39f8862bd085e63efd4e2b9e396afa0c543
tappyness1 <neocezheng@outlook.com>
2023-02-14 18:35:22 +0800

    changes to workflow

--- src/inference.py
+++ src/inference.py
@@ -1,6 +1,7 @@
 from peekingduck.pipeline.nodes.model import yolo as pkd_yolo
 import cv2
 from collections import defaultdict
+import numpy as np
 
 def get_counts(inference_outputs):
     """helper function to count the objects belonging to each class
@@ -31,10 +32,36 @@ class inference:
         self.yolo_node = pkd_yolo.Node(model_type = "v4", detect= ["car", "motorcycle", "truck", "bus"], )
         
     def run_inference(self, img_path):
+        """use if img_path is specified 
+
+        Args:
+            img_path (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
         image_orig = cv2.imread(img_path)
         image_orig = cv2.cvtColor(image_orig, cv2.COLOR_BGR2RGB)
 
         yolo_input = {"img": image_orig}
+        yolo_output = self.yolo_node.run(yolo_input)
+        output_counts_dict = get_counts(yolo_output)
+
+        return output_counts_dict
+    
+    def run_inference_direct(self, img_bytes):
+        """use if the img_bytes is specified instead of path
+
+        Args:
+            img_bytes (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        img_decoded = cv2.imdecode(np.frombuffer(img_bytes, np.uint8), -1)
+        img_decoded = cv2.cvtColor(img_decoded, cv2.COLOR_BGR2RGB)
+
+        yolo_input = {"img": img_decoded}
         yolo_output = self.yolo_node.run(yolo_input)
         output_counts_dict = get_counts(yolo_output)
 
--- src/pull_data.py
+++ src/pull_data.py
@@ -8,6 +8,25 @@ def _process_text(text):
     text = text.replace(" ", "_")
     return text
 
+def _process_data_to_path(card):
+    img_link = card.find("img").get("src")
+    img_url =  f"http:{img_link}"
+    text = _process_text(card.find("img").get("alt"))
+    img_data = requests.get(img_url).content 
+    with open(f'data/{text}.jpg', 'wb') as handler: 
+        handler.write(img_data) 
+    return text
+
+def _process_data_direct(card):
+    img_link = card.find("img").get("src")
+    img_url =  f"http:{img_link}"
+    text = _process_text(card.find("img").get("alt"))
+    img_data = requests.get(img_url).content 
+    return text, img_data
+
+def _get_name_only(card):
+    text = _process_text(card.find("img").get("alt"))
+
 def get_data(cfg_obj):
     """gets the data from the website and downloads them into the data folder
 
@@ -30,18 +49,32 @@ def get_data(cfg_obj):
     # for each card in cards, extract the img link
     # download the image using the img link 
     # process the text of the img text to be used as the img file name
-
     img_fnames = []
     for card in cards:
-        img_link = card.find("img").get("src")
-        img_url =  f"http:{img_link}"
-        text = _process_text(card.find("img").get("alt"))
+        text = _process_data_to_path(card)
         img_fnames.append(f"{text}.jpg")
-        img_data = requests.get(img_url).content 
-        with open(f'data/{text}.jpg', 'wb') as handler: 
-            handler.write(img_data) 
 
     return img_fnames
 
+def get_data_direct(cfg_obj):
+        # request data source website
+    r = requests.get(cfg_obj['data_source']) 
+
+    # convert to beautiful soup 
+    soup = bs(r.content, features="html.parser")
+
+    # cards is the class of the div that stores the image url
+    cards = soup.find_all("div", class_= "card")
+
+    img_dict = {}
+    for card in cards:
+        text, img_data = _process_data_direct(card)
+        img_dict[text] = img_data 
+
+    return img_dict
+
+
 if __name__=='__main__':
-    print(get_data())
+    cfg_path="config/config.yml"
+    cfg_obj = open_yaml_file(fname = cfg_path)
+    print(get_data(cfg_obj))
